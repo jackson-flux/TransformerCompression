5c82bca9542f356ea2be8f9e2c685a3756dccc09
Henry Jackson-Flux <hjacksonflux@microsoft.com>
2024-09-03 14:00:19 +0100

    Eval identical to Pashmina's

--- experiments/run_lm_eval.py
+++ experiments/run_lm_eval.py
@@ -14,7 +14,9 @@ from lm_eval import utils as lm_eval_utils
 from lm_eval.api.registry import ALL_TASKS
 from lm_eval.models.huggingface import HFLM
 from lm_eval.tasks import initialize_tasks
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
+from quarot.model_phi35 import Phi3ForCausalLM
 from slicegpt import gpu_utils, hf_utils, utils
 from slicegpt.config import config
 
@@ -38,7 +40,6 @@ TASK_METRIC_MAP = {
 
 
 def eval_arg_parser(interactive: bool = True) -> argparse.Namespace:
-    initialize_tasks()
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--model",
@@ -81,7 +82,6 @@ def eval_arg_parser(interactive: bool = True) -> argparse.Namespace:
         '--tasks',
         nargs='+',
         default=["piqa", "hellaswag", "arc_easy", "arc_challenge", "winogrande"],
-        choices=lm_eval_utils.MultiChoice(tasks.ALL_TASKS),
     )
     parser.add_argument('--num-fewshot', type=int, default=0, help="Number of fewshots for all tasks.")
     parser.add_argument("--save-dir", type=str, default=".", help="Path to save the lm eval results")
@@ -118,6 +118,20 @@ def calculate_avg_accuracy(task_names: str, results: dict) -> float:
 
     return (acc_cumul + acc_mmlu_avg) / (n_tasks - len(questions_per_mmlu_task) + 1)
 
+def run_lm_eval(
+    hflm: HFLM, task_list: list, fewshot: int, batch_size: int, fraction: float, output_file: str, log_msg: str
+):
+    results = lm_eval.simple_evaluate(
+        hflm, tasks=task_list, num_fewshot=fewshot, batch_size=batch_size, limit=fraction
+    )['results']
+    metrics = {task: round(result.get('acc_norm,none', result['acc,none']), 4) for task, result in results.items()}
+    metrics['acc_avg'] = round(sum(metrics.values()) / len(metrics.values()), 4)
+    metrics['num_fewshot'] = fewshot
+    metrics['limit'] = fraction
+    logging.info(f"{log_msg} {metrics}")
+    with open(output_file, "w") as f:
+        json.dump(metrics, f)
+
 
 def eval_main(args: argparse.Namespace) -> None:
     logging.info("Running SliceGPT LM eval experiment.")
@@ -133,66 +147,43 @@ def eval_main(args: argparse.Namespace) -> None:
         logging.info(f'Failed to initialize wandb: {e}, continuing without wandb')
         wandb.init(project=args.wandb_project, mode='disabled')
 
-    if args.sliced_model_path:
-        # load the sliced model
-        logging.info(f"Loading sliced {args.model} model from {args.sliced_model_path} with sparsity {args.sparsity}")
-        model_adapter, tokenizer = hf_utils.load_sliced_model(
-            args.model,
-            args.sliced_model_path,
-            sparsity=args.sparsity,
-            token=args.hf_token,
-            round_interval=args.round_interval,
-        )
-    else:
-        # load the original model
-        logging.info(f"Loading {args.model} model")
-        model_adapter, tokenizer = hf_utils.get_model_and_tokenizer(args.model, args.model_path, token=args.hf_token)
+    model = Phi3ForCausalLM.from_pretrained(args.model_path, torch_dtype="auto",
+                                                 trust_remote_code=True, local_files_only=True,
+                                                 attn_implementation="flash_attention_2")  # or eager for onnx
+    model.to(config.device)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_path, local_files_only=True, trust_remote_code=True)
 
     # the lm eval harness ties the weights, but this should not be done for sliced models unless the lm_head was sliced
-    model_adapter.model.tie_weights = lambda: None
-
-    if args.distribute_model:
-        # distribute model across available GPUs
-        gpu_utils.distribute_model(model_adapter)
-    else:
-        model_adapter.model.to(config.device)
+    model.tie_weights = lambda: None
 
     ### LM Eval Harness ###
-    hflm = HFLM(pretrained=model_adapter.model, tokenizer=tokenizer, batch_size=args.batch_size)
+    hflm = HFLM(pretrained=model, tokenizer=tokenizer, batch_size=args.batch_size)
 
-    if args.tasks is None:
-        task_names = tasks.ALL_TASKS
-    else:
-        task_names = lm_eval_utils.pattern_match(args.tasks, ALL_TASKS)
+    initialize_tasks()
+    task_names = lm_eval_utils.pattern_match(args.tasks, ALL_TASKS)
 
-    logging.info(f"Selected Tasks: {task_names}")
-
-    for task in task_names:
-        if task not in TASK_METRIC_MAP:
-            raise NotImplementedError(
-                f"Please specify the metric to use for {task} in TASK_METRIC_MAP. Available info {TASK_METRIC_MAP}"
-            )
-
-    results = lm_eval.simple_evaluate(hflm, tasks=task_names, num_fewshot=args.num_fewshot, batch_size=args.batch_size)[
-        'results'
+    mmlu_tasks = [
+        "mmlu_abstract_algebra",
+        "mmlu_business_ethics",
+        "mmlu_college_computer_science",
+        "mmlu_college_mathematics",
+        "mmlu_conceptual_physics",
+        "mmlu_formal_logic",
+        "mmlu_machine_learning",
+        "mmlu_miscellaneous",
+        "mmlu_philosophy",
+        "mmlu_global_facts",
     ]
 
-    logging.info(results)
-    wandb.log(results)
-
-    with open(f"{args.save_dir}/full_results_{args.num_fewshot}_shot.json", "w") as f:
-        json.dump(results, f)
-
-    metric_vals = {task: round(result.get(TASK_METRIC_MAP[task]), 4) for task, result in results.items()}
-    acc_avg = calculate_avg_accuracy(task_names, results)
-    metric_vals['average'] = round(acc_avg, 4)
-    with open(f"{args.save_dir}/{args.num_fewshot}_shot_task_results.json", "w") as f:
-        json.dump(metric_vals, f)
-
-    wandb.log({'acc_avg': acc_avg})
-
-    logging.info(json.dumps(metric_vals, indent=4))
-    logging.info(f"Average accuracy across tasks: {acc_avg}")
+    run_lm_eval(
+        hflm,
+        task_list=task_names,
+        batch_size=args.batch_size,
+        fewshot=args.num_fewshot,
+        fraction=1,
+        output_file=f"{args.save_dir}/lm_eval.json",
+        log_msg="LM Eval results (limit=1): ",
+    )
 
 
 if __name__ == "__main__":
